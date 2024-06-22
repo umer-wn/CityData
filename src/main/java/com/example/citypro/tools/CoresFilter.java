f8e62a08708b25c37d44028773f36cc513ddc983
tjcolinliu <2478167310@qq.com>
2024-06-22 16:17:56 +0800

    部分修改，修正了跨域问题

---
+++ src/main/java/com/example/citypro/tools/CoresFilter.java
@@ -0,0 +1,22 @@
+package com.example.citypro.tools;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CoresFilter {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // 允许所有来源
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
